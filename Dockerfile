6a81f6b6c3767860cb6b82aaea57e3f914e6a7ed
Bence Dányi <bence@danyi.me>
2015-01-22 16:48:44 +0100

    Refactor image

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM mbentley/nginx
 MAINTAINER Purpose Industres <dev@purposeindustries.co>
-ADD nginx.conf /etc/nginx/nginx.conf
 ADD run.sh /etc/nginx/run.sh
 CMD /etc/nginx/run.sh
--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# generate cert
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/nginx.key -out /etc/nginx/nginx.crt -subj "/C=/ST=/L=/O=/CN=/"
-
 # link to sinopia container
 echo "upstream sinopia { server $SINOPIA_PORT_4873_TCP_ADDR:$SINOPIA_PORT_4873_TCP_PORT; }"> /etc/nginx/sinopia.conf
 
 # start nginx
-nginx -c /etc/nginx/nginx.conf
+nginx -c /opt/sinopia/nginx.conf
